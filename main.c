97e9d78781ec26ae23f4eb55fba1471a314eabb1
Delannoy Théo <theo.delannoy74@gmail.com>
2026-03-13 15:10:06 +0100

    tests main

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct{
+typedef struct _liste_rec {
     int val;
-    struct liste_rec *suiv;
+    struct _liste_rec *suiv; // On utilise le nom "struct node" ici
 } liste_rec;
 
 liste_rec* creer_liste(int val){
@@ -14,16 +14,12 @@ liste_rec* creer_liste(int val){
     return l;
 }
 
-bool vide(liste_rec* l){
-    return (l->val == NULL && l->suiv == NULL);
-}
-
 int premier(liste_rec l){
     return l.val;
 }
 
-liste_rec* reste(liste_rec l){
-    return l.suiv;
+liste_rec* reste(liste_rec* l){
+    return l->suiv;
 }
 
 liste_rec* prefixer(liste_rec* l, int val){
@@ -32,8 +28,8 @@ liste_rec* prefixer(liste_rec* l, int val){
     }
     else{
         liste_rec* l2 = creer_liste(val);
-        l2->suiv = l;
-        return l2;
+        l->suiv = l2;
+        return l;
     }
     
 }
@@ -53,4 +49,38 @@ liste_rec* suffixer(liste_rec* l, int val){
         suffixer(l->suiv, val);
     }
     return l;
+}
+
+void affiche_liste(liste_rec* l){
+    if (!vide(l)){
+        printf("%d", l->val);
+        affiche_liste(l->suiv);
+    }
+    printf("\n");
+}
+
+liste_rec* fusion(liste_rec* l1, liste_rec* l2) {
+    if (l1 == NULL) return l2;
+    if (l2 == NULL) return l1;
+
+    // On choisit le plus petit élément comme tête de liste
+    if (l1->val <= l2->val) {
+        l1->suiv = fusion(l1->suiv, l2);
+        return l1;
+    } else {
+        l2->suiv = fusion(l1, l2->suiv);
+        return l2;
+    }
+}
+
+int main(){
+    liste_rec* l1 = creer_liste(7);
+    liste_rec* l2 = creer_liste(5);
+
+    suffixer(l1, 9);
+    prefixer(l2, 3);
+
+    liste_rec* l3 = fusion(l1, l2);
+
+    affiche_liste(l3);
 }
