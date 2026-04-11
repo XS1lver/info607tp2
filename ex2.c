6b314d64f13e1625e6b80f198bec38d48e7a60f2
Delannoy Théo <theo.delannoy74@gmail.com>
2026-04-11 16:23:48 +0200

    Questions 2.2 et 2.3

--- ex2.c
+++ ex2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+
+#define BASE 10
+#define MODULO 1000003 // Grand nombre premier pour limiter les collisions
 
 int bruteForce(char* text, char* motif){
     int i = 0;
@@ -26,18 +30,75 @@ int bruteForce(char* text, char* motif){
     }
 }
 
+int hash(char* str) {
+    int val = 0;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        val = (val * BASE + (str[i] - '0')) % MODULO;
+    }
+    return val;
+}
+
+int RabinKarp(char* text, char* motif){
+    int i = 0;
+    int m = strlen(motif);
+    int n = strlen(text);
+    bool trouve = false;
+    int hashm = hash(motif);
+
+    char sous_chaine[m + 1];
+
+    while (!trouve && i <= n - m){
+        strncpy(sous_chaine, &text[i], m);
+        sous_chaine[m] = '\0';
+
+        if (hashm == hash(sous_chaine)){
+            if (strncmp(&text[i], motif, m) == 0) {
+                trouve = true;
+            }
+            else{
+                i = i + 1;
+            }
+        }
+        else{
+            i = i + 1;
+        }
+    }    
+}
+
 int main(){
-    char text[] = "1230495830123";
-    char pattern[] = "301";
+    int n = 10000000;
+    char* text = malloc(n + 1);
+    char motif[] = "534872941";
+    int m = strlen(motif);
 
-    printf("Recherche du motif '%s' dans '%s' :\n", pattern, text);
-    int result = bruteForce(text, pattern);
+    // Génération d'un texte aléatoire
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        text[i] = (rand() % 10) + '0';
+    }
+    text[n] = '\0';
 
-    if (result != -1){
-        printf("Motif trouvé à l'indice %d\n", result);
-    }
-    else{
-        printf("Motif non trouvé\n");
-    }
+    strcpy(&text[n - m], motif); // Motif placé à la fin pour forcer le pire cas de l'algo
+
+    printf("\nTest de performance pour Brute force:\n");
+    clock_t start_brute_force = clock();
+    int result = bruteForce(text, motif);
+    clock_t end_brute_force = clock();
+    double time_brute_force = ((double)(end_brute_force - start_brute_force)) / CLOCKS_PER_SEC;
+    printf("Motif trouvé à l'indice %d\n", result);
+    printf("Temps: %f\n", time_brute_force);
+
+
+    printf("Test de performance pour Rabin Karp:\n");
+    clock_t start_RK = clock();
+    int result_RK = RabinKarp(text, motif);
+    clock_t end_RK = clock();
+    double time_RK = ((double)(end_RK - start_RK)) / CLOCKS_PER_SEC;
+    printf("Motif trouvé à l'indice %d\n", result_RK);
+    printf("Temps: %f\n", time_RK);
+
+
+    free(text);
     return 0;
 }
