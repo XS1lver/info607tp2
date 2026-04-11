0fe7c41123218690cdba636af177d72cd97a3c2c
Delannoy Théo <theo.delannoy74@gmail.com>
2026-04-11 14:03:35 +0200

    Question 1.4

--- main.c
+++ main.c
@@ -84,10 +84,20 @@ liste_rec* SSC(liste_rec* l){
     return res;
 }
 
-/*
-liste_rec* comp(liste_rec A, liste_rec B){
-
-}*/
+// Définie la sous-suite complément de la suite A dans la suite B
+liste_rec* comp(liste_rec* A, liste_rec* B){
+    if (A == NULL) {
+        return B; // Si A est vide on retourne B
+    }
+    if (B == NULL) {
+        return NULL; // Si seulement B est vide, alors A ne commence pas B
+    }
+    if (A->val == B->val) { // Si les éléments de A et de B sont égaux, on continue la comparaison avec les éléments suivants
+        return comp(A->suiv, B->suiv);
+    }
+    // Si A ne commence pas B, retourne NULL
+    return NULL;
+}
 
 int main(){
     liste_rec* l1 = creer_liste(7);
@@ -113,4 +123,16 @@ int main(){
     printf("\nSSC: ");
     liste_rec* l4 = SSC(l3);
     affiche_liste(l4);
+
+    printf("\ncomp: ");
+    liste_rec* lA = creer_liste(4); // Création de la liste A
+    suffixer(lA, 7);
+    suffixer(lA, 9);    
+    liste_rec* lB = creer_liste(4); // Création de la liste B
+    suffixer(lB, 7);
+    suffixer(lB, 9);
+    suffixer(lB, 1);
+    suffixer(lB, 2);
+    liste_rec* l5 = comp(lA, lB); // Calcul de la sous-suite complément de A dans B
+    affiche_liste(l5);
 }
